b35287e949fbe86236d4dc0c9e09097243a9eca2
最是光阴化浮沫 <zsgyhfm@icloud.com>
2017-03-01 16:37:17 +0800

    完成加载本地数据

---
+++ TODOLIST/data.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yishan on 17/3/1.
+ */
+var data = {
+    task:[
+        {
+            taskId:'1',//唯一索引
+            taskContent:'初次加载',//内容
+            taskIscomplete:false,//是否完成
+        },
+        {
+            taskId:'2',//唯一索引
+            taskContent:'15:33还没吃饭',//内容
+            taskIscomplete:true,//是否完成
+        }
+    ]
+}
